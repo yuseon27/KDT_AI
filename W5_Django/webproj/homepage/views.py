ba8c6258aeb7e0800c67668ce9288f4c68dc4dca
yuseon27 <ysnam19@naver.com>
2021-05-13 12:13:05 +0900

    W5D2 - Form을 생성해서 template을 통해 DB 수정하기

--- W5_Django/webproj/homepage/views.py
+++ W5_Django/webproj/homepage/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from .models import Coffee
-'''
-render : html 문서 관리 함수
-render(request, '.html', {})
+from .forms import CoffeeForm
 
-'''
 # Create your views here.
 def index(request) : #{
     # return HttpResponse("<h1>Hello World!</h1>")
@@ -16,5 +13,15 @@ def index(request) : #{
 
 def coffee_view(request) : #{
     coffee_all = Coffee.objects.all()   ## SELECT * FROM Coffee   ## .get(), .fiter(), .....
-    return render(request, 'coffee.html', {"coffee_list":coffee_all})    
+    # 만약 request가 POST라면 :
+        # POST를 바탕으로 Form을 완성하고
+        # Form이 유효하면 -> DB에 저장!
+    if request.method == "POST" :
+        form = CoffeeForm(request.POST)  ## 새로운, 완성된 Form을 만들고
+        if form.is_valid() :             ## 채워진 form 안에 있는 값이 유효한지 확인
+            form.save()                  ## 이 Form을 Model에 저장
+
+
+    form = CoffeeForm()
+    return render(request, 'coffee.html', {"coffee_list":coffee_all, "coffee_form":form})    
 #}
