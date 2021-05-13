8577ff6641a0756eba84b1711e57a4d584803a0f
yuseon27 <ysnam19@naver.com>
2021-05-13 10:57:46 +0900

    W5D1 Template Language - variable, filter, tag

--- W5_Django/webproj/homepage/views.py
+++ W5_Django/webproj/homepage/views.py
@@ -7,5 +7,8 @@ render(request, '.html', {})
 # Create your views here.
 def index(request) : #{
     # return HttpResponse("<h1>Hello World!</h1>")
-    return render(request, 'index.html', {})
+    number = 50
+    name   = 'Michael'
+    nums   = [1, 2, 3, 4, 5]
+    return render(request, 'index.html', {"my_num":number, "my_name":name, "my_list":nums})
 #}
