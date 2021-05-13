87a14b61f9834f3ec3d49d54cae24a00803cf561
yuseon27 <ysnam19@naver.com>
2021-05-13 10:20:10 +0900

    W5D1 django introduction

---
+++ W5_Django/webproj/homepage/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+'''
+render : html 문서 관리 함수
+render(request, '.html', {})
+
+'''
+# Create your views here.
+def index(request) : #{
+    # return HttpResponse("<h1>Hello World!</h1>")
+    render(request, 'index.html', {})
+#}
